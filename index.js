dbbdbd75789236263b5bbdb1b2a5ee1be8f55cea
Nana Ewusi <mrewusi@gmail.com>
2017-03-21 14:18:03 +0000

    Revert to zipping to a contained directory

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ class S3Downloader {
   unzipApp() {
     let zipPath = this.zipPath;
 
-    return this.exec('unzip ' + zipPath)
+    return this.exec('unzip ' + zipPath + ' -d ' + this.outputPath)
       .then(() => {
         this.ui.writeLine("unzipped " + zipPath);
       });
